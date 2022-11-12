a2eb7c3e3ba57c07705fcccff1ecbcd1bde6400b
TarasTikan <tikantaras1@gmail.com>
2022-11-12 23:24:41 +0200

    add dadasd

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,9 @@
 const colorBtn = document.querySelector('.change-color')
-const colorEl = document.querySelector('color')
+const colorEl = document.querySelector('.color')
 const bodyEl = document.querySelector('body')
 colorBtn.addEventListener('click', getRandomHexColor)
 function getRandomHexColor() {
-   const bg = `#${Math.floor(Math.random() * 16777215).toString(16)}`
-   return bodyEl.style.backgroundColor = bg
- return colorEl.textContent = `Change color ${bg}`
+  const hex = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+   bodyEl.style.backgroundColor = hex
+   colorEl.textContent = `${hex}`
 }
-
-// const colorBtn = document.querySelector('.change-color')
-// const colorEl = document.querySelector('color')
-// return colorEl.textContent = event.currentTarget.colorBtn
