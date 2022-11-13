c2650180360bf881f44400643c4889141cc9eeed
TarasTikan <tikantaras1@gmail.com>
2022-11-13 20:42:39 +0200

    add fasf

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,7 @@ inputEl.addEventListener('blur', (event) => {
     if(event.currentTarget.value.length == inputLength){
         inputEl.classList.remove('invalid');
         inputEl.classList.add('valid');
-    }  else if (event.currentTarget.value.length > inputLength) {
-        inputEl.classList.remove('valid')
-        inputEl.classList.add('validation-input')
-    }
+    }  
     else {
         inputEl.classList.remove('valid');
         inputEl.classList.add('invalid');     
